f67249d04ab16d7f8ebf9d831d849f0a8f11add2
sunzhiqiangGithub <731265487@qq.com>
2018-09-02 15:44:41 +0800

    spring cloud study仓库初始化

---
+++ src/main/java/cn/com/sunzhiqiang/microservice/SpringCloudStudyApplication.java
@@ -0,0 +1,12 @@
+package cn.com.sunzhiqiang.microservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudStudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudStudyApplication.class, args);
+	}
+}
